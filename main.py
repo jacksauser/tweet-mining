1a475d2dac1e84a7fb90dba7834b4741a3ff17d1
jacksauser <91438713+jacksauser@users.noreply.github.com>
2023-01-22 14:49:00 -0600

    update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import sys
-
+import json
+import nltk
+import re
 
 # collecting the award show queried and year
 award_show = input("What award show? ")
@@ -13,4 +15,28 @@ if not year:
     year = 2013
 else:
     year = int(year)
+
+
+def word_frew(l):
+    
+
+
+
+#opening the data
+data = json.load(open('gg2013.json'))
+
+
+
+regex_host = r'(?i)host'
+host_strings = []
+
+
+for i in data:
+    t = i['text']
+    u = i['user']
+    if re.search(regex_host, t) and u['screen_name'] != 'goldenglobes':
+        host_strings.append(t)
+
+
+
 
