a52300590e28e4dd6ba2563b39e18bb2e675e8d3
karthik1405 <58023216+karthik1405@users.noreply.github.com>
2023-02-03 15:10:08 -0600

    Update Checker.py

--- Checker.py
+++ Checker.py
@@ -1,24 +1,26 @@
 import imdb
 from fuzzywuzzy import fuzz
 
-# create an instance of the IMDb class
-
-
 # get the string input
 #actor = input("Enter an actor name: ")
 #movie = input("Enter movie name: ")
 
 class checker:
     
+    # Create dictionaries for movies and actors
     actorDict = {}
     movieDict = {}
-
+    
+    # create an instance of the IMDb class
     ia = imdb.IMDb()
 
+    # Initialize
     def __init__(self, name, year):
         self.showName = name
         self.year = year
 
+    # Checks if a name is an actor by accessing IMDb
+    # Returns the IMDb matched name if found, 0 if no match, and -1 if there is a timeout
     def isActor(self, name):
         try:
             # search for the actor
@@ -33,6 +35,7 @@ class checker:
             if str(e) == "timeout":
                 return -1
 
+    # Same as isActor above, but for movies
     def isMovie(self, name):
         try:
             search_results = self.ia.search_movie(name)
@@ -46,6 +49,12 @@ class checker:
             if str(e) == "timeout":
                 return -1
 
+    # Checks the dictionaries to see if a name exists
+    # If found, returns the value which is the name or a 0
+    # If not found, uses isActor and isMovie to find nearest match and compares string similarity
+    # If input string and IMDb string are similar enough, updates value with the official name (typo checker)
+    # After adding new elements to the dictionary, checks again for initial string and returns the value
+    # This function will either output the official name of an actor/movie or a 0
     def checkDict(self, input):
         a = self.actorDict.get(input, -1)
         m = self.movieDict.get(input, -1)
@@ -53,11 +62,12 @@ class checker:
         # If an actor or a movie is not in the dictionary and is close enough to
         # the IMDb name, it gets added to the dictionary with the name as the value.
         # Otherwise, it gets a 0, meaning no match or too broad
+        # This uses Levenshtein distance and accepts anything above a 70% similarity
         if a == -1:
             act = self.isActor(input)
             if act and act != -1:
                 rat = fuzz.ratio(input, act)
-                if rat<=5:
+                if rat>=70:
                     self.actorDict[input] = act
             else:
                 self.actorDict[input] = 0
@@ -66,7 +76,7 @@ class checker:
             mov = self.isMovie(input)
             if mov and mov != -1:
                 rat = fuzz.ratio(input, mov)
-                if rat<=5:
+                if rat>=70:
                     self.movieDict[input] = mov
             else:
                 self.movieDict[input] = 0
@@ -82,26 +92,11 @@ class checker:
             return a2
         elif m2 != 0 and m2 != -1:
             return m2
-        elif a2 == 0 and m2 == 0:
-                return False
-
-
-actor = input("Enter an actor name: ")
-check = checker('gg',2018)
+        else:
+            return 0
+        
+# Quick testing function
+actor = input("Enter a name: ")
+check = checker('Golden Globes',2013)
 a = check.checkDict(actor)
-#a = check.isActor(actor)
 print(a)
-
-
-# a = isActor(actor)
-# m = isMovie(movie)
-# if a and a != -1:
-#     if m and m != -1:
-#         print(a,"and",m)
-#     else:
-#         print(a)
-# else:
-#     if m and m != -1:
-#         print(m)
-#     else:
-#         print('None')
