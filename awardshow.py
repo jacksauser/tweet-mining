d3264706ca0dd818ce2a42e9957409321abb7bff
jacksauser <91438713+jacksauser@users.noreply.github.com>
2023-02-04 20:06:23 -0600

    update to AwardShow class

--- awardshow.py
+++ awardshow.py
@@ -9,11 +9,22 @@ class AwardShow:
     
     showName = ""
     year = ""
-    host = ""
+    host = []
     awards = []
 
     def __init__(self, name, year):
         self.showName = name
         self.year = year
 
+    def print_readable(self):
+        print()
+
+    def print_json(self):
+        print()
+
+    def addHost(self, s):
+        self.host.append(s)
     
+    def addAward(self, a):
+        if isinstance(a, Award):
+            self.awards.append(a)
